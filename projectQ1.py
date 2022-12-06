098cb8cec2b59a4fdc947d627e33f62836ad82c9
Dawit <abesha24@gmail.com>
2022-12-06 15:05:30 -0500

    python

---
+++ projectQ1.py
@@ -0,0 +1,22 @@
+#1) Create a simple Python program application that does the following:
+
+    # Prints "Hello User!"
+
+    # Then asks "What is your name?"
+
+    # Then responds "Hello <user's name>"
+
+    # Then asks: "What is your year of birth?"
+
+    # Then asks: "Do you know how to drive?"
+
+    # Then asks: "How old were you when you got your driving license" if the user replies "Yes" to the above question
+
+    # Then outputs: "You got your driving license in <year> and you have been driving for <x> years" 
+
+    # Then outputs: "You are <age> years old now but you still can't drive. 
+    #   You were supposed to get your license in <year>,<x> years ago at the age of 30 at the latest" If the user replies "No" and is over the age of 30
+
+    # Then outputs: "You are <age> years old now and you should work on getting your driving license before you turn 30. 
+    # You have <x> years to go until the year <year>" If the user replies "No" and is under the age of 30
+
